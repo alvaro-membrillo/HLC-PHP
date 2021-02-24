c8a9d73b598543197e02f148756f0b84a921228c
= <=>
2021-02-24 16:37:45 +0100

    Tarea10_Arrays

---
+++ Tarea10_Arrays.php
@@ -0,0 +1,20 @@
+<?php
+
+$array_escalar[0] = "Domingo";
+$array_escalar[1] = "Lunes";
+$array_escalar[2] = "Martes";
+$array_escalar[3] = "Miércoles";
+$array_escalar[4] = "Jueves";
+$array_escalar[5] = "Viernes";
+$array_escalar[6] = "Sábado";
+
+$array_asociativo["Primero"] = "Domingo";
+$array_asociativo["Segundo"] = "Lunes";
+$array_asociativo["Tercero"] = "Martes";
+$array_asociativo["Cuarto"] = "Miércoles";
+$array_asociativo["Quinto"] = "Jueves";
+$array_asociativo["Sexto"] = "Viernes";
+$array_asociativo["Séptimo"] = "Sábado";
+
+echo $array_escalar[0];
+echo $array_asociativo["Tercero"];
