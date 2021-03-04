4e9860ebc3135af59bdcf9cce4c70a9483093901
= <=>
2021-03-04 13:45:29 +0100

    FuncionConParametros

---
+++ FuncionParametro.php
@@ -0,0 +1,13 @@
+<?php
+echo "<pre>";
+function HolaMundo($var2)
+{
+    echo $var2;
+}
+
+echo HolaMundo("Hola Alex") . "\n";
+
+echo HolaMundo("Hola Luisa") . "\n";
+
+echo HolaMundo("Hay que pasarle un parametro para resolver el error");
+?>
