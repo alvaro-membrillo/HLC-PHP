d49174ae6a49b8401374abca6801e76effd3018a
= <=>
2021-03-04 18:05:35 +0100

    Proyecto php y mysql

---
+++ Proyecto_PHP_MySQL/galeria/common/config.php
@@ -0,0 +1,12 @@
+<?php
+
+  $config = array(
+    
+    'database' => array(
+      'host' => 'localhost',
+      'username' => 'devuser',
+      'password' => 'mysql',
+      'database' => 'GALLERY2',
+      'encoding' => 'utf8',
+    ),
+  );
