a4efd32de47abe150900e355c66ad5e0f10930f5
= <=>
2021-03-04 01:23:27 +0100

    get_post_ejercicios

---
+++ GET_POST/formularioGET.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <title>
+    </title>
+</head>
+<body>
+    <form method=get action=get_post.php>
+        Nombre: <input type="text" name="usuario">
+        <br>
+        <input type ="Submit" name"Enviar" value="Enviar">
+    </form>
+
+    <!--Get muestra la informacion del input que se envia en la URL-->
+</body>
+</html>
---
+++ GET_POST/formularioPOST.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <title>
+    </title>
+</head>
+<body>
+     
+    <form method=post action=get_post.php>
+        Nombre: <input type="text" name="usuario">
+        <br>
+        <input type ="Submit" name"Enviar" value="Enviar">
+    </form>
+
+    <!--Post no muestra la informacion que se envia en el input en la URL-->
+    
+</body>
+</html>
