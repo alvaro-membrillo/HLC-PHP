1bade4872fa9834714fe86f95d37dffcc3c055c1
= <=>
2021-03-04 01:10:57 +0100

    switch

---
+++ switch.php
@@ -0,0 +1,11 @@
+<?php
+
+switch ($i) {
+    case 0: //Si es = 0 aparece este texto
+        echo "i es igual a 0";
+    case 1: //Si es = 1 aparece este texto
+        echo "i es igual a 1";
+    case 2: //Si es = 2 aparece este texto
+        echo "i es igual a 2";
+}
+?>
